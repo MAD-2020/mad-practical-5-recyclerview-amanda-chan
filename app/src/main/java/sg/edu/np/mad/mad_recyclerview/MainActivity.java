d94048a09aac8914cdd9f3de64be103f73f81482
Hui En Amanda Chan <s10196608@connect.np.edu.sg>
2020-06-04 15:27:57 +0800

    Practical 5

--- app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
@@ -1,20 +1,58 @@
 package sg.edu.np.mad.mad_recyclerview;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> taskList = new ArrayList<>();
+    private EditText new_task;
+    final String TAG = "To-Do List";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button add_button = findViewById(R.id.addButton);
+
+        new_task = findViewById(R.id.newTask);
+
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final PracticalAdapter pAdapter = new PracticalAdapter(taskList);
+        LinearLayoutManager pLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(pLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(pAdapter);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input_task();
+                pAdapter.notifyDataSetChanged();
+                showNewEntry(recyclerView, taskList);
+            }
+        });
+
+    }
+    public void input_task() {
+        String new_data = new_task.getText().toString();
+        taskList.add(new_data);
     }
 
     /**
